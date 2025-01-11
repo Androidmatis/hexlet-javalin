b948f194c52cec08541ad4fac9f92fed00158555
Androidmatis <androidmatis@gmail.com>
2025-01-12 00:47:40 +0600

    Шаблонизатор

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     id("java")
     application
+    id("io.freefair.lombok") version "8.4"
 }
 
 application {
@@ -23,11 +24,14 @@ dependencies {
     // Здесь мы сразу подключаем зависимости,
     // которые понадобятся во время обучения
     implementation("io.javalin:javalin:6.1.3")
+    implementation("net.datafaker:datafaker:2.0.1")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("io.javalin:javalin-rendering:6.1.3")
     implementation("gg.jte:jte:3.1.9")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin-rendering:6.1.3")
 }
 
 tasks.test {
--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,18 +1,52 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import org.example.hexlet.dto.courses.CoursePage;
+import org.example.hexlet.dto.courses.CoursesPage;
+import org.example.hexlet.model.Course;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+        List<Course> courses = new ArrayList<>();
+        courses.add(new Course("java", "java dev"));
+        courses.add(new Course("php", "php dev"));
+        courses.add(new Course("python", "python dev"));
+        courses.add(new Course("ST", "ST dev"));
+        for (int i = 0; i < courses.size(); i++) {
+            courses.get(i).setId((long) (i + 1));
+        }
+        //app.get("/courses/{id}", ctx -> {
+        //    var id = ctx.pathParamAsClass("id", Long.class);
+        //    var course = courses.stream().filter(c -> Objects.equals(c.getId(), id.get())).findFirst().get();
+        //            // Предполагаем, что у курса есть метод getName()
+        //            ctx.result("<h1>" + course.getName() + "</h1>");
+        //});
+
+
+        app.get("/courses/{id}", ctx -> {
+            var id = ctx.pathParamAsClass("id", Long.class);
+            var course = courses.stream().filter(c -> Objects.equals(c.getId(), id.get())).findFirst().get();
+            var page = new CoursePage(course);
+            ctx.render("courses/show.jte", model("page", page));
         });
 
-        // Название параметров мы выбрали произвольно
-        app.get("/users/{id}/post/{postId}", ctx -> {
-            var usersd = ctx.pathParam("id");
-            var postId =  ctx.pathParam("postId");
-            ctx.result("User ID: " + usersd + "\nPost ID: " + postId);
+
+        app.get("/courses", ctx -> {
+            var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", model("page", page));
         });
 
         app.start(7070);
